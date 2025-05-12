e14385988c0c7c7363d3f77ea97f35e15a6d877e
Denisse Chagoya <denissechagoyaveloz@gmail.com>
2025-05-11 19:06:47 -0600

    diseño de la card para animales

--- app/src/main/java/com/example/chogoyadenisseanimalsapp/components/AnimalItemCard.kt
+++ app/src/main/java/com/example/chogoyadenisseanimalsapp/components/AnimalItemCard.kt
@@ -1,2 +1,84 @@
 package com.example.chogoyadenisseanimalsapp.components
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.FavoriteBorder
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import coil3.compose.AsyncImage
+
+@Composable
+fun AnimalItemCard(
+    name: String,
+    imageUrl: String,
+    modifier: Modifier = Modifier
+) {
+    Surface(
+        modifier = modifier
+            .width(150.dp)
+            .height(180.dp),
+        shape = RoundedCornerShape(20.dp),
+        color = Color.White,
+        shadowElevation = 4.dp
+    ) {
+        Box(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(12.dp)
+        ) {
+            Column(
+                horizontalAlignment = Alignment.CenterHorizontally,
+                modifier = Modifier
+                    .fillMaxSize()
+            ) {
+                AsyncImage(
+                    model = imageUrl,
+                    contentDescription = name,
+                    modifier = Modifier
+                        .size(80.dp)
+                        .clip(RoundedCornerShape(50)),
+                    contentScale = ContentScale.Crop
+                )
+
+                Spacer(modifier = Modifier.height(12.dp))
+
+                Text(
+                    text = name,
+                    style = MaterialTheme.typography.bodyMedium,
+                    color = Color.Black
+                )
+            }
+
+            Icon(
+                imageVector = Icons.Default.FavoriteBorder,
+                contentDescription = "Favorito",
+                tint = Color.Gray,
+                modifier = Modifier
+                    .align(Alignment.TopEnd)
+                    .size(20.dp)
+            )
+        }
+    }
+}
+
+@Preview(showBackground = false)
+@Composable
+fun AnimalItemCardPreview() {
+    AnimalItemCard(
+        name = "Nutria",
+        imageUrl = "https://i.redd.it/04rwloagnx551.jpg"
+    )
+}
