97230676cdc2f5ef22b595513c35aaff0bd236f9
Denisse Chagoya <denissechagoyaveloz@gmail.com>
2025-05-11 19:29:07 -0600

    diseño de la card para environments

--- app/src/main/java/com/example/chogoyadenisseanimalsapp/components/EnvironmentItemCard.kt
+++ app/src/main/java/com/example/chogoyadenisseanimalsapp/components/EnvironmentItemCard.kt
@@ -1,2 +1,78 @@
 package com.example.chogoyadenisseanimalsapp.components
 
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.FavoriteBorder
+import androidx.compose.material3.*
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import coil3.compose.AsyncImage
+
+@Composable
+fun EnvironmentItemCard(
+    name: String,
+    imageUrl: String,
+    modifier: Modifier = Modifier
+) {
+    Surface(
+        modifier = modifier
+            .width(150.dp)
+            .height(180.dp),
+        shape = RoundedCornerShape(20.dp),
+        color = Color.White,
+        shadowElevation = 4.dp
+    ) {
+        Box(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(12.dp)
+        ) {
+            Column(
+                horizontalAlignment = Alignment.CenterHorizontally,
+                modifier = Modifier.fillMaxSize()
+            ) {
+                AsyncImage(
+                    model = imageUrl,
+                    contentDescription = name,
+                    modifier = Modifier
+                        .size(80.dp)
+                        .clip(RoundedCornerShape(50)),
+                    contentScale = ContentScale.Crop
+                )
+
+                Spacer(modifier = Modifier.height(12.dp))
+
+                Text(
+                    text = name,
+                    style = MaterialTheme.typography.bodyMedium,
+                    color = Color.Black
+                )
+            }
+
+            Icon(
+                imageVector = Icons.Default.FavoriteBorder,
+                contentDescription = "Favorito decorativo",
+                tint = Color.Gray,
+                modifier = Modifier
+                    .align(Alignment.TopEnd)
+                    .size(20.dp)
+            )
+        }
+    }
+}
+
+@Preview(showBackground = false)
+@Composable
+fun EnvironmentItemCardPreview() {
+    EnvironmentItemCard(
+        name = "Selva Tropical",
+        imageUrl = "https://images.pexels.com/photos/19678111/pexels-photo-19678111/free-photo-of-paisaje-bosque-naturaleza-selva.jpeg"
+    )
+}
